0d2c320816cca7c27c411bc573f3021ba6d8e0bc
Archermystery <84961834+Archermystery@users.noreply.github.com>
2024-08-13 02:52:08 +0300

    create install.sh

---
+++ script/install.sh
@@ -0,0 +1,14 @@
+chmod +x start
+pacman -S --needed git base-devel wofi waybar hyprland kitty firefox yazi mako imv mpv zathura hyprlock bottom telegram-desktop discord
+cd ~
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd .. 
+rm -rf yay
+yay -S wofi-calc wofi-wifi-menu-git 
+systemctl --user start startapp.service
+
+#Vencord
+sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
+
