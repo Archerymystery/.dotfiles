b6fbcb97d73b796bd85501d8a99831f1f0d2f48c
Archerymystery <84961834+Archerymystery@users.noreply.github.com>
2024-08-14 02:03:46 +0300

    Update install.sh

--- script/install.sh
+++ script/install.sh
@@ -1,5 +1,5 @@
 chmod +x start
-pacman -S --needed git base-devel wofi waybar hyprland kitty firefox yazi mako imv mpv zathura hyprlock bottom telegram-desktop discord thefuck
+pacman -S --needed git base-devel wofi waybar hyprland kitty firefox yazi mako imv mpv zathura hyprlock bottom telegram-desktop nodejs discord nvim pnpm thefuck
 stow .
 cd ~
 git clone https://aur.archlinux.org/yay.git
@@ -11,5 +11,12 @@ yay -S wofi-calc wofi-wifi-menu-git
 systemctl --user start startapp.service
 
 #Vencord
-sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
-
+git clone https://github.com/Vendicated/Vencord.git
+cd Vencord/src
+mkdir userplugins
+cd userplugins/
+git clone https://github.com/henmalib/vc-messageColors.git
+git clone https://github.com/nyakowint/vc-premid.git
+cd ../..
+pnpm i
+pnpm build && pnpm inject
