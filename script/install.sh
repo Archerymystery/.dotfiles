b3da6115e17b23582b15c13dfce5a3c947d91e69
Archerymystery <84961834+Archerymystery@users.noreply.github.com>
2024-10-04 21:58:30 +0300

    add cloning tmux plagins to install script

--- script/install.sh
+++ script/install.sh
@@ -1,3 +1,4 @@
+cd ~/.dotfiles
 chmod +x start
 pacman -S --needed git base-devel wofi waybar hyprland kitty firefox yazi mako imv mpv zathura hyprlock bottom telegram-desktop nodejs discord nvim pnpm thefuck
 stow .
@@ -11,7 +12,17 @@ rm -rf yay
 yay -S wofi-calc wofi-wifi-menu-git 
 systemctl --user start startapp.service
 
+
+#tmux
+cd ~/.dotfiles/.config/tmux
+mkdir plugins
+cd plugins 
+git clone https://github.com/tmux-plugins/tpm.git
+git clone https://github.com/nordtheme/tmux.git
+git clone https://github.com/tmux-plugins/tmux-yank
+git clone https://github.com/tmux-plugins/tmux-resurrect
 #Vencord
+cd ~/
 git clone https://github.com/Vendicated/Vencord.git
 cd Vencord/src
 mkdir userplugins
